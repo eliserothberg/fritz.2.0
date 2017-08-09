787fecbc7e31a19d3ba4567b6939dc2b3c9e3898
eliserothberg <rothberg.elise@gmail.com>
2017-08-09 15:59:41 -0700

    adjusted contact page

--- src/containers/Contact.js
+++ src/containers/Contact.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, FormGroup, FormControl, ControlLabel, Col } from 'react-bootstrap';
+import { Button, FormGroup, FormControl, ControlLabel, Col, Row } from 'react-bootstrap';
 import './Contact.css';
 
 function FieldGroup({ id, label, help, ...props }) {
@@ -15,40 +15,43 @@ class Contact extends Component {
   render() {
     return (
       <div id="contact">
-        <Col xs={12}>
-        <h1>Contact Us</h1>
+        <Row>
+            <h1>Contact Us</h1>
 
-      <form>
+            <form>
+            <Col md={2} mdOffset={5}>
 
-      <br></br>
-      <FieldGroup
-          id="formControlsName"
-          type="text"
-          label="Name"
-          placeholder="Enter your name"
-        />
-        <FieldGroup
-          id="formControlsEmail"
-          type="email"
-          label="Email address"
-          placeholder="Enter your email"
-        />
-        
-         <FormGroup controlId="formControlsTextarea">
-          <ControlLabel>Questions or comments:</ControlLabel>
-          <FormControl componentClass="textarea" placeholder="How can we help you?" />
-        </FormGroup>
-
-        <FieldGroup
-          id="formControlsFile"
-          type="file"
-          label="Upload any relevant files"
-        />
-        <Button type="submit">
-          Submit
-        </Button>
-      </form>
-      </Col>
+            <br></br>
+            <FieldGroup
+              id="formControlsName"
+              type="text"
+              label="Name"
+              placeholder="Enter your name"
+            />
+            <FieldGroup
+              id="formControlsEmail"
+              type="email"
+              label="Email address"
+              placeholder="Enter your email"
+            />
+            </Col>
+            <Col md={6} mdOffset={3}>
+            <FormGroup controlId="formControlsTextarea">
+              <ControlLabel>Questions or comments:</ControlLabel>
+              <FormControl componentClass="textarea" placeholder="How can we help you?" />
+            </FormGroup>
+            </Col>
+            <Col md={2} mdOffset={5}>
+            <FieldGroup
+              id="formControlsFile"
+              type="file"
+              label="Upload any relevant files"
+            />
+            
+            <Button type="submit">Submit</Button>
+            </Col>
+            </form>
+        </Row>
       </div>
     );
   }
