f4fe8caf4686659c1ce689ee3c69d60ba8c3da3c
eliserothberg <rothberg.elise@gmail.com>
2017-08-10 15:03:26 -0700

    added video to home page, started styling services

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
 import './Home.css';
+import countryside from '.././img/countryside.mp4';
+
 
 class Home extends Component {
-  render() {
+  render () {
     return (
-      <div className="Home">
+      <div id="whole-page">
+       <div className="Home">
         <div className="lander">
           <h1>Fritz Land Survey, LLC</h1>
           <p>no bible quotes</p>
-         </div>
+        </div>
       </div>
-    );
+       
+      <video className='videoTag' autoPlay loop muted plays-inline>
+        <source src={countryside} type='video/mp4' />
+      </video>
+      </div>
+    )
   }
 }
 
--- src/containers/Services.js
+++ src/containers/Services.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import { Grid, Row, Col } from 'react-bootstrap';
+import { Grid, Row, Col, Image, Media } from 'react-bootstrap';
 import './Services.css';
+// import '.././img/landUseMap.jpg';
+import landUseMap from '../img/landUseMap.jpg';
 
 class Services extends Component {
   render() {
@@ -9,52 +11,120 @@ class Services extends Component {
         <div id="services">
          <Col>
             <h1>Our Services</h1>
-          <Grid>
-            <Row>
-              <Col md={4}><p>Preliminary Survey</p>
-                    <h4>The collection of survey data on which to base studies on a proposed project 
-                     a proposed final survey.</h4></Col>
-              <Col md={4}><p>Boundary and Stake Survey</p>
-                    <h4>A survey performed to establish or re-establish a boundary line on the ground 
-                    or to obtain data for constructing a map or plat showing a boundary line.</h4></Col>
-              <Col md={4}><p>Construction Survey</p>
-                    <h4>The survey measurements made while construction is in process to control elevation, 
-                    horizontal position and dimensions, and to determine adequacy of completion.</h4></Col>
-            </Row>
-            <Row>
-              <Col md={4}><p>Topographic Survey</p>
-                    <h4>A survey of an area which has for its major purposes and determination of the 
-                    configuration of the surface of the ground (using elevation contours) and the location 
-                    of the natural and artificial objects.</h4></Col>
-              <Col md={4}><p>Site Planning and Design Survey</p>
-                    <h4>A combination of boundary and topographic surveys with the resulting information 
-                    being used for designing development features such as roads, subdivisions, utilities, 
-                    buildings, etc.</h4></Col>  
-              <Col md={4}><p>Mortgage Inspection Report</p>
-                    <h4>A report made for the sole use of a lending institution to evaluate title problems 
-                    relating to possession. The Mortgage Inspection Report shows the relationship of the 
-                    improvements to the approximate property lines and filed easements or rights of way.  
-                    Survey makers are not usually set with this type of survey.</h4></Col>
 
-              </Row>
-            <Row>
-              <Col md={4}><p>American Land and Title Association (ALTA)<br></br>Title Survey </p>
-                    <h4>A survey of real property, including the plat of survey, acceptable to a title 
-                    insurance company for purposes of insuring title to said real property, free and clear 
-                    of survey questions, except those questions disclosed by the survey and indicated on 
-                    the plat.</h4></Col>
-              <Col md={4}><p>Elevation Survey</p>
-                    <h4>A survey of a property, building or residence and its relation to a flood plain.  
-                    Elevation surveys are used by lending agencies and insurance companies to determine the need for flood insurance or if a site is suitable for a new structure.</h4></Col>
-              <Col md={4}><p>Subdivision Survey</p>
-                    <h4>A type of land survey in which the legal boundaries of an area are located and the 
-                    area is divided into parcels of lots, streets, rights-of-way and other accessories.  
-                    All necessary corners or dividing lines are marked or monumented.</h4></Col>
-            </Row>
-          </Grid>
-        </Col>
+            <Media>
+              <Media.Left align="middle">
+                <img width={110} height={110} src={landUseMap} alt="land use map"/>
+              </Media.Left>
+              <Media.Body>
+                <Media.Heading>Preliminary Survey</Media.Heading>
+                <p>The collection of survey data on which to base studies on a proposed project 
+                             a proposed final survey.</p>
+              </Media.Body>
+            </Media>
+            
+            <Media>
+              <Media.Body>
+                <Media.Heading>Boundary and Stake Survey</Media.Heading>
+                <p>A survey performed to establish or re-establish a boundary line on the ground 
+                      or to obtain data for constructing a map or plat showing a boundary line.</p>
+              </Media.Body>
+
+              <Media.Right align="middle">
+                <img width={110} height={110} src={landUseMap} alt="Image"/>
+              </Media.Right>
+            </Media>
+            
+            <Media>
+              <Media.Left align="middle">
+                <img width={110} height={110} src={landUseMap} alt="land use map"/>
+              </Media.Left>
+              
+              <Media.Body>
+                <Media.Heading>Construction Survey</Media.Heading>
+                <p>The survey measurements made while construction is in process to control elevation, 
+                    horizontal position and dimensions, and to determine adequacy of completion.</p>
+              </Media.Body>
+            </Media>
+            
+            <Media>
+              <Media.Body>
+                <Media.Heading>Topographic Survey</Media.Heading>
+                <p>A survey of an area which has for its major purposes and determination of the 
+                            configuration of the surface of the ground (using elevation contours) and the location 
+                            of the natural and artificial objects.</p>
+              </Media.Body>
+              
+              <Media.Right align="middle">
+                <img width={110} height={110} src={landUseMap} alt="Image"/>
+              </Media.Right>
+            </Media>
+            
+            <Media>
+            <Media.Left align="middle">
+                <img width={110} height={110} src={landUseMap} alt="land use map"/>
+              </Media.Left>
+
+              <Media.Body>
+                <Media.Heading>Site Planning and Design Survey</Media.Heading>
+                <p>A combination of boundary and topographic surveys with the resulting information being 
+                used for designing development features such as roads, subdivisions, utilities, buildings,
+                 etc.</p>
+              </Media.Body>
+             </Media>
+            
+            <Media>
+              <Media.Body>
+                <Media.Heading>Mortgage Inspection Report</Media.Heading>
+                <p>A report made for the sole use of a lending institution to evaluate title problems 
+                relating to possession. The Mortgage Inspection Report shows the relationship of the 
+                improvements to the approximate property lines and filed easements or rights of way. 
+                Survey makers are not usually set with this type of survey.</p>
+              </Media.Body>
+              <Media.Right align="middle">
+                <img width={110} height={110} src={landUseMap} alt="Image"/>
+              </Media.Right>
+            </Media>
+            
+            <Media>
+            <Media.Left align="middle">
+                <img width={110} height={110} src={landUseMap} alt="land use map"/>
+              </Media.Left>
+
+              <Media.Body>
+                <Media.Heading>American Land and Title Association (ALTA) Title Survey</Media.Heading>
+                <p>A survey of real property, including the plat of survey, acceptable to a title insurance 
+                company for purposes of insuring title to said real property, free and clear of survey 
+                questions, except those questions disclosed by the survey and indicated on the plat.</p>
+              </Media.Body>
+             </Media>
+            
+            <Media>
+              <Media.Body>
+                <Media.Heading>Elevation Survey</Media.Heading>
+                <p>A survey of a property, building or residence and its relation to a flood plain. 
+                Elevation surveys are used by lending agencies and insurance companies to determine 
+                the need for flood insurance or if a site is suitable for a new structure.</p>
+              </Media.Body>
+              <Media.Right align="middle">
+                <img width={110} height={110} src={landUseMap} alt="Image"/>
+              </Media.Right>
+            </Media>
+            
+            <Media>
+            <Media.Left align="middle">
+                <img width={110} height={110} src={landUseMap} alt="land use map"/>
+              </Media.Left>
+              <Media.Body>
+                <Media.Heading>Subdivision Survey</Media.Heading>
+                <p>A type of land survey in which the legal boundaries of an area are located and the 
+                area is divided into parcels of lots, streets, rights-of-way and other accessories. 
+                All necessary corners or dividing lines are marked or monumented.</p>
+              </Media.Body>
+            </Media>
+          </Col>
+        </div>
       </div>
-    </div>
     );
   }
 }
